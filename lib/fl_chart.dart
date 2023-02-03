1f96254b18d9faa819459174d57ebcaf6efc2fec
lovefreeUnity <84775180+lovefreeUnity@users.noreply.github.com>
2023-02-03 18:38:46 +0900

    export tooltipMode enum
    패키지를 만들 때 export를 해주지 않으면 안된다는 것을 처음 알았다 라이브러리를 만들 때 라이브러리 이름과 사용자가 사용할 수 있는 파일의 위치를 입력해주어야 한다는 점을 새롭게 알게 되었다,

--- lib/fl_chart.dart
+++ lib/fl_chart.dart
@@ -8,6 +8,7 @@ export 'src/chart/base/base_chart/base_chart_data.dart';
 export 'src/chart/base/base_chart/fl_touch_event.dart';
 export 'src/chart/line_chart/line_chart.dart';
 export 'src/chart/line_chart/line_chart_data.dart';
+export 'src/chart/line_chart/tooltip_mode.dart';
 export 'src/chart/pie_chart/pie_chart.dart';
 export 'src/chart/pie_chart/pie_chart_data.dart';
 export 'src/chart/radar_chart/radar_chart.dart';
